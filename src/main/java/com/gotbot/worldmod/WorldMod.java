220bdf1199d55a8ba4d8a88dd9f7f6ad7142c87e
gotbot12345 <box12345man@gmail.com>
2022-03-11 07:02:03 +0000

    Item System

--- src/main/java/com/gotbot/worldmod/WorldMod.java
+++ src/main/java/com/gotbot/worldmod/WorldMod.java
@@ -1,10 +1,12 @@
 package com.gotbot.worldmod;
 
+import com.gotbot.worldmod.item.WorldModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -28,7 +30,11 @@ public class WorldMod
     public WorldMod()
     {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        WorldModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/com/gotbot/worldmod/item/WorldModItems.java
+++ src/main/java/com/gotbot/worldmod/item/WorldModItems.java
@@ -1,5 +1,16 @@
 package com.gotbot.worldmod.item;
 
+import com.gotbot.worldmod.WorldMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class WorldModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WorldMod.MOD_ID);
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
 }
