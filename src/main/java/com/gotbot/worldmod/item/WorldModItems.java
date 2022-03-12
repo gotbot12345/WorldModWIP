2b96e1801e8473a14ea59e635eb53d19fe33b400
gotbot12345 <box12345man@gmail.com>
2022-03-12 11:16:45 +0000

    Silver Ingot

--- src/main/java/com/gotbot/worldmod/item/WorldModItems.java
+++ src/main/java/com/gotbot/worldmod/item/WorldModItems.java
@@ -1,13 +1,17 @@
 package com.gotbot.worldmod.item;
 
 import com.gotbot.worldmod.WorldMod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class WorldModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WorldMod.MOD_ID);
+
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus)
     {
